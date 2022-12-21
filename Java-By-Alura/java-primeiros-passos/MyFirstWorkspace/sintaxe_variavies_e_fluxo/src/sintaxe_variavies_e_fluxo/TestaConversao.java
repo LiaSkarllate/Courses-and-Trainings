6c42e322291b035a45b6f767bc5608e03bf673a3
LiaSkarllate <liaskarllate@protonmail.com>
2022-12-20 21:58:23 -0300

    feat: My first Eclipse workspace created.

---
+++ Java-By-Alura/java-primeiros-passos/MyFirstWorkspace/sintaxe_variavies_e_fluxo/src/sintaxe_variavies_e_fluxo/TestaConversao.java
@@ -0,0 +1,16 @@
+package sintaxe_variavies_e_fluxo;
+
+public class TestaConversao {
+	public static void main(String args[]) {
+		double salario = 1270.50;
+		int valor = (int) salario; //É preciso ter cuidado ao converter tipos, dado que o compilador apenas o deixa fazê-lo explicitamente.
+		
+		System.out.println(valor);
+		
+		double valorPromovido = 3; //Isso é uma promoção de tipo: de inteiro para ponto flutuante.
+	
+		long numeroGrande = 8323983901283091283l;
+		short numeroPequeno = 2422;
+		byte numeroMenorAinda = 120;
+	}
+}
