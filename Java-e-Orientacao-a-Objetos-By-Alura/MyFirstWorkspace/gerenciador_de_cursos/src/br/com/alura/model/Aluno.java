459b5d14aa9b9e99e3b73ef5c69c824d2354a303
LiaSkarllate <liaskarllate@protonmail.com>
2023-10-03 17:53:51 -0300

    feat: Eighth course completed.

---
+++ Java-e-Orientacao-a-Objetos-By-Alura/MyFirstWorkspace/gerenciador_de_cursos/src/br/com/alura/model/Aluno.java
@@ -0,0 +1,39 @@
+package br.com.alura.model;
+
+public class Aluno {
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		// Isso é um exemplo de programação defensiva:
+		if (nome == null) {
+			throw new NullPointerException("Nome não pode ser nulo");
+		}
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+	@Override
+	public String toString() {
+		return "[Aluno: " + this.nome + ", matricula: " + this.numeroMatricula + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Aluno outroAluno = (Aluno) obj;
+		return this.nome.equals(outroAluno.nome);
+	}
+
+	@Override
+	public int hashCode() {
+		return this.nome.hashCode();
+	}
+}
